7313aacac877aa03e3bb00ee7855d0cfedf986c3
Andrew Hartwig <andrew.hartwig1@gmail.com>
2016-02-26 22:36:46 -0500

    Holy mother of progress

--- Shifts/Shifts/Shifts.cs
+++ Shifts/Shifts/Shifts.cs
@@ -7,16 +7,27 @@ namespace Shifts
 {
 	public class App : Application
 	{
+		public const string INTRO_FINISHED = "finished";
+
 		public App ()
 		{
-			MainPage = new SetupPageCarousel ();
-
-			SetupPage6.Finished += IntroFinished;
+			try {
+				string app_status = (string)Properties ["app_status"];
+				if (app_status == INTRO_FINISHED) {
+					MainPage = new CalendarOverviewPage ();
+				}
+			} catch (KeyNotFoundException KnfE) {
+				MainPage = new SetupPageCarousel ();
+				SetupPage6.Finished += IntroFinished;
+			}
+				
 		}
 
 		private void IntroFinished (object sender, EventArgs e)
 		{
-			Debug.WriteLine ("Finished!");
+			MainPage = new CalendarOverviewPage ();
+			SetupPage6.Finished -= IntroFinished;
+			Properties ["app_status"] = "finished";
 		}
 
 		protected override void OnStart ()
