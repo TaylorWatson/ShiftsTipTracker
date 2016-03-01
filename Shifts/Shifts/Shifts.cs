95f384806b5c7931d14166e949b548fd78298dcd
Andrew Hartwig <andrew.hartwig1@gmail.com>
2016-03-01 01:13:44 -0500

    Readded tutorial

--- Shifts/Shifts/Shifts.cs
+++ Shifts/Shifts/Shifts.cs
@@ -25,8 +25,8 @@ namespace Shifts
 			} catch (KeyNotFoundException KnfE) {
 
 				String s = KnfE.Message; // Just to suppress 'KnfE never used' error message.
-				//MainPage = new SetupPageCarousel ();
-				MainPage = new NavigationPage(new CalendarOverviewPage ());
+				MainPage = new SetupPageCarousel ();
+//				MainPage = new NavigationPage(new CalendarOverviewPage ());
 				SetupPage6.Finished += IntroFinished;
 
 			}
